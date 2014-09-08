2e5b9c567e36962bdb87979532f28611175dd992
Gabriel Buades <gbuades@soffid.com>
2014-09-08 13:44:37 +0200

    IAM-446 Add translations

--- sayakacp/src/main/native/ShiroProvider.cpp
+++ sayakacp/src/main/native/ShiroProvider.cpp
@@ -84,6 +84,10 @@ HRESULT ShiroProvider::SetUsageScenario(
         break;
 
     case CPUS_CREDUI:
+    	s_rgShiroFieldDescriptors[SHI_TITLE].pszLabel =  wcsdup ( MZNC_strtowstr(Utils::LoadResourcesString(45).c_str()).c_str() );
+    	s_rgShiroFieldDescriptors[SHI_USER].pszLabel =  wcsdup ( MZNC_strtowstr(Utils::LoadResourcesString(46).c_str()).c_str() );
+    	s_rgShiroFieldDescriptors[SHI_PASSWORD].pszLabel =  wcsdup ( MZNC_strtowstr(Utils::LoadResourcesString(47).c_str()).c_str() );
+    	s_rgShiroFieldDescriptors[SHI_MESSAGE].pszLabel =  wcsdup ( MZNC_strtowstr(Utils::LoadResourcesString(48).c_str()).c_str() );
     	hr = S_OK;
         break;
 
