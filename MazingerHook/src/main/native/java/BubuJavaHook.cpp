2e7755b515e468b8b013f53de9ec6a6e903bab1d
Gabriel Buades <gbuades@soffid.com>
2014-09-22 12:34:24 +0200

    Fix wrong ProfYumi path on IE32 on Windows 64 bits

--- MazingerHook/src/main/native/java/BubuJavaHook.cpp
+++ MazingerHook/src/main/native/java/BubuJavaHook.cpp
@@ -490,10 +490,41 @@ void uninstallJavaPlugin() {
 	}
 }
 
+typedef BOOL (WINAPI *LPFN_ISWOW64PROCESS)(HANDLE, PBOOL);
+
+static LPFN_ISWOW64PROCESS fnIsWow64Process = NULL;
+
+BOOL IsWow64()
+{
+	BOOL bIsWow64 = FALSE;
+
+	//IsWow64Process is not available on all supported versions of Windows.
+	//Use GetModuleHandle to get a handle to the DLL that contains the function
+	//and GetProcAddress to get a pointer to the function if available.
+	if (fnIsWow64Process == NULL)
+		fnIsWow64Process = (LPFN_ISWOW64PROCESS) GetProcAddress(
+				GetModuleHandle(TEXT("kernel32")), "IsWow64Process");
+
+	if (NULL != fnIsWow64Process)
+	{
+		if (!fnIsWow64Process(GetCurrentProcess(), &bIsWow64))
+		{
+			//handle error
+		}
+	}
+
+	return bIsWow64;
+}
+
+static DWORD Wow64Key(DWORD flag)
+{
+	return flag | (IsWow64() ? KEY_WOW64_64KEY : 0);
+}
+
 const char* getMazingerDir() {
 	HKEY hKey;
 	if (RegOpenKeyEx(HKEY_LOCAL_MACHINE,
-			"SOFTWARE\\Microsoft\\Windows\\CurrentVersion", 0, KEY_READ, &hKey)
+			"SOFTWARE\\Microsoft\\Windows\\CurrentVersion", 0, Wow64Key(KEY_READ), &hKey)
 			== ERROR_SUCCESS) {
 		static char achPath[4096] = "XXXX";
 		DWORD dwType;
