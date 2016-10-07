a741afe128a234bc13f3bc0f62995a6f386be118
Gabriel Buades <gbuades@soffid.com>
2016-10-07 13:48:50 +0200

    Install new firefox addon

--- installer-deb/src/main/resources/usr/lib/firefox-addons/extensions/{df382936-f24b-11df-96e1-9bf54f13e327}/default/preferences/prefs.js
+++ installer-deb/src/main/resources/usr/lib/firefox-addons/extensions/{df382936-f24b-11df-96e1-9bf54f13e327}/default/preferences/prefs.js
@@ -1,2 +1,2 @@
 // https://developer.mozilla.org/en/Localizing_extension_descriptions
-pref("extensions.df382936-f24b-11df-96e1-9bf54f13e327.description", "chrome://MazingerExtension/locale/overlay.properties");
+pref("extensions.df382936-f24b-11df-96e1-9bf54f13e327.description", "chrome://SoffidESSOExtension/locale/overlay.properties");
